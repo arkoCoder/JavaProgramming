353675bae2c609cc1dc83960d5a9c29a080f0ee8
Gopal Heda <gopal.heda1@hp.com>
2021-05-29 21:41:35 +0530

    Initial Commit

---
+++ src/main/java/com/basics/java/Identifiers.java
@@ -0,0 +1,21 @@
+package com.basics.java;
+/*
+ * - Identifiers are names in java
+ * - Identifiers can only have below characters:
+ *   - a-z, A-Z, 0-9, $ , _
+ * - Identifiers should always start with alphabets
+ * - Identifiers are case-sensitive
+ * - No length limit for java identifiers
+ * - Reserved words cannot be used as identifiers
+ * - Predefined java class names and interface names can be used as identifiers.
+ * */
+
+public class Identifiers {
+
+    public static void main(String[] args) {
+        int String = 1;
+        System.out.println(String);
+
+    }
+
+}
