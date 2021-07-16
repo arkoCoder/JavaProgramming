04203f2be773c65974a195a8385ddedcf236b5ab
Gopal Heda <gopal.heda1@hp.com>
2021-07-16 22:45:40 +0530

    More collection framework

---
+++ src/main/java/com/basics/java/fundamentals/collections/CollectionsComparable.java
@@ -0,0 +1,5 @@
+package com.basics.java.fundamentals.collections;
+
+public class CollectionsComparable {
+
+}
---
+++ src/main/java/com/basics/java/fundamentals/collections/CollectionsComparator.java
@@ -0,0 +1,9 @@
+package com.basics.java.fundamentals.collections;
+
+/*
+ * Comparable:
+ * - Provides customizable sorting order
+ * */
+public class CollectionsComparator {
+
+}
