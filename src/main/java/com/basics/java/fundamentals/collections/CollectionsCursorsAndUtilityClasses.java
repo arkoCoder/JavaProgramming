87015e3108ce160190ee8b6859a70f86a3769804
Gopal Heda <gopal.heda1@hp.com>
2021-07-16 22:50:52 +0530

    More on Collections

---
+++ src/main/java/com/basics/java/fundamentals/collections/CollectionsCursorsAndUtilityClasses.java
@@ -0,0 +1,17 @@
+package com.basics.java.fundamentals.collections;
+
+/*
+ * Cursors:
+ * - Used to fetch object from collections one by one
+ * - Types:
+ *   - Enumeration
+ *   - Iterator
+ *   - ListIteration
+ * 
+ * Utility Classes:
+ * - Collections
+ * - Arrays
+ * */
+public class CollectionsCursorsAndUtilityClasses {
+
+}
